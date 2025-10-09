aee042f76bf76152ce77c561fd2c8092ceb32763
Iván Iarunichev <ivani94@educastur.es>
2025-10-09 16:46:46 +0200

    new-commit

---
+++ src/ColaPedidos.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ColaPedidos {
+    private Queue<Integer> cola = new LinkedList<Integer>();
+    private int capacidad;
+
+    public synchronized void nuevoPedido(int pedido) throws InterruptedException {
+        //Si la cola esta llena, se espera
+        while(cola.size() == capacidad){
+            //Espera si la colaPedidos esta lleno
+            wait();
+        }
+
+        //Si hay especio se añade a la cola
+        cola.add(pedido);
+        System.out.println("Pedido cola: " + pedido);
+
+        //Avisamos a los consumidores
+        notifyAll();
+    }
+
+    public synchronized String procesarPedido() throws InterruptedException {
+        //Mientras la cola esta vacia el trajador debe esperar
+        while(cola.isEmpty()){
+            //El trabajador debe esperar
+            wait();
+        }
+        String pedido = String.valueOf(cola.poll());
+        System.out.println("Pedido cola: " + pedido);
+
+        //Avisamos al trabajador de que hay pedido
+        notifyAll();
+
+        return pedido;
+    }
+    public Queue<Integer> getCola() {
+        return cola;
+    }
+}
---
+++ src/ProductorPedidos.java
@@ -0,0 +1,30 @@
+public class ProductorPedidos extends Thread {
+    private ColaPedidos colaPedidos; //colaPedidos compartido
+    private String nombreCliente;
+
+    //Constructor compartido
+    public ProductorPedidos(ColaPedidos colaPedidos, String nombreCliente) {
+        this.colaPedidos = colaPedidos;
+        this.nombreCliente = nombreCliente;
+    }
+
+    @Override
+    public void run() {
+        try {
+            //Bucle generando como ejemplo 100 pedidos
+            for (int i = 1; i <= 100; i++) {
+                //Y por cada pedido se tendrá un nombre con el siguiente estilo
+                String pedido = "Pedido " + i + " de " + nombreCliente;
+
+                //Lo mostramos por pantalla
+                System.out.println(pedido);
+                colaPedidos.nuevoPedido(i);
+
+                //Despues de cada pedido se duerme un tiempo
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
